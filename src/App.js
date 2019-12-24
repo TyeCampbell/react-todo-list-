30a317a35424d27fcde6d6e501ce73b2e4a4a24a
Tye Campbell <tylerjoelcampbell@gmail.com>
2019-12-24 09:01:54 -0600

    added intial structure to react app. Todo component, TodoList component, and TodoForm component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import TodoList from './TodoList'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>The Todo App</h1>
+      <TodoList/>
     </div>
   );
 }
